25dd6eb473fb217a4d6a64ef6d5a79d4a0528570
Ismael Patrick <ismael.patrick@belmicro.com.br>
2023-11-09 17:24:56 -0300

    Icone home

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import { Home } from "../pages/home";
 import { Search } from "../pages/search";
@@ -10,8 +11,29 @@ const Tab = createBottomTabNavigator();
 
 export function Routes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#000",
+          borderTopWidth: 0,
+        },
+        tabBarActiveTintColor: "#FFFFFF",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused, size, color }) => {
+            if (focused) {
+              return <Ionicons name="home" size={size} color={color} />;
+            }
+            return <Ionicons name="home-outline" size={size} color={color} />;
+          },
+        }}
+      />
       <Tab.Screen name="Search" component={Search} />
       <Tab.Screen name="New" component={New} />
       <Tab.Screen name="Inbox" component={Inbox} />
